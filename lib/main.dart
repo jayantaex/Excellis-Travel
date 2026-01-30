d5b9b22763846bae5005dbb003d5e1fd0c8d9623
Jayanta <jayanta.mahato@excellisit.net>
2026-01-30 15:42:33 +0530

    style: Remove an unnecessary blank line from `main.dart`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
