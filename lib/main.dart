7a6c767fcdc30cc0d5d062796bf74419a9faf5f4
Jayanta <jayanta.mahato@excellisit.net>
2026-02-13 19:30:54 +0530

    refactor: remove unused import statements for improved code clarity

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'dart:io';
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show SystemChrome, DeviceOrientation;
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'core/localization/supported_local.dart';
 import 'core/services/download_manager.dart';
@@ -17,13 +15,6 @@ import 'app_router.dart';
 void main() async {
   const bool firebaseSetup = true;
   WidgetsFlutterBinding.ensureInitialized();
-
-  if (!kProfileMode) {
-    await SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
-  }
-
   await StorageService.init();
   await LocalDB().initLocalDB();
   await EasyLocalization.ensureInitialized();
